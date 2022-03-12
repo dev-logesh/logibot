748da86ea24b340406745dcf285b486f5040e4e7
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-03-12 22:10:30 +0000

    Update chat_status.py

--- logibot/modules/helper_funcs/chat_status.py
+++ logibot/modules/helper_funcs/chat_status.py
@@ -403,6 +403,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from Tianabot.modules import connection
+from logibot.modules import connection
 
 connected = connection.connected
