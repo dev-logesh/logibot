578633604e4b0d05a1407b0d4eecfe8867bd0955
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-03-13 09:00:47 +0000

    Update currency_converter.py

--- logibot/modules/currency_converter.py
+++ logibot/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from Tianabot import CASH_API_KEY, dispatcher
+from logibot import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
