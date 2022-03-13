301c9ec94c8f6a05962019bb5cc39cc368008f40
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-03-13 05:02:11 +0000

    Update admin.py

--- logibot/modules/admin.py
+++ logibot/modules/admin.py
@@ -15,7 +15,7 @@ from logibot.modules.helper_funcs.chat_status import (
     user_admin,
     ADMIN_CACHE,
 )
-from Tianabot.helper_extra.admin_rights import (
+from logiabot.helper_extra.admin_rights import (
     user_can_pin,
     user_can_promote,
     user_can_changeinfo,
