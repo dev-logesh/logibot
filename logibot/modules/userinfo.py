32a1b6e2a2f7dee2a1b473d377743c561af31f78
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-03-13 06:44:25 +0000

    Update userinfo.py

--- logibot/modules/userinfo.py
+++ logibot/modules/userinfo.py
@@ -586,7 +586,7 @@ dispatcher.add_handler(GET_BIO_HANDLER)
 dispatcher.add_handler(SET_ABOUT_HANDLER)
 dispatcher.add_handler(GET_ABOUT_HANDLER)
 
-__mod_name__ = "Iɴғᴏs"
+__mod_name__ = ".✨Iɴғᴏs✨"
 __command_list__ = ["setbio", "bio", "setme", "me", "info"]
 __handlers__ = [
     ID_HANDLER,
