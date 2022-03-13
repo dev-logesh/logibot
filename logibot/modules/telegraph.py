68e15e2a8feec3fb849acff37e5fafe1dadece4d
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-03-13 06:48:22 +0000

    Update telegraph.py

--- logibot/modules/telegraph.py
+++ logibot/modules/telegraph.py
@@ -6,7 +6,7 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-darkprince = "MASHA"
+darkprince = "LOGI"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=darkprince)
 auth_url = r["auth_url"]
