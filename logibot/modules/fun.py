8b2c2bdcecc2a3768aad46a90aa2e9e68a67c0f6
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-03-13 07:05:31 +0000

    Update fun.py

--- logibot/modules/fun.py
+++ logibot/modules/fun.py
@@ -328,7 +328,7 @@ dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
 dispatcher.add_handler(WEEBIFY_HANDLER)
 
-__mod_name__ = "Fᴜɴ"
+__mod_name__ = "✨Fᴜɴ✨"
 __command_list__ = [
     "runs", "slap", "roll", "toss", "shrug", "bluetext", "rlg", "decide",
     "table", "pat", "sanitize", "lyrics", "weebify",
