b01cb9346b71079b38ce4ab2cd8894489aaa6897
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-03-14 11:05:01 -0700

    Update google.py

--- logibot/modules/google.py
+++ logibot/modules/google.py
@@ -267,7 +267,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> Tiana Bot <==="
+        app_details += "\n\n===> LOGI Bot <==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
